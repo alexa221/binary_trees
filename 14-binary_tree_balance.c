b07cc77ae8f6bf5ce096b9bfbca88480e25b7b52
alexa221 <alextakele16@gmail.com>
2022-09-21 16:38:08 +0300

    balance binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,6 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (0);
 }
+
 /**
  * binary_tree_balance - measures the balance factor of a binary tree.
  *@tree: pointer to the root node of the tree to measure the balance factor.
