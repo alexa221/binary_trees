0378108db1fbfa168b65b741e3c973a2efe0e199
alexa221 <alextakele16@gmail.com>
2022-09-21 15:48:21 +0300

    delete entire BT

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,9 @@
+
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - function that deletes an entire binary tree
+ * binary_tree_delete - function that deletes an entire binary tree using c Programming
+ *
  *@tree: pointer to the root node of the tree to delete.
  */
 void binary_tree_delete(binary_tree_t *tree)
