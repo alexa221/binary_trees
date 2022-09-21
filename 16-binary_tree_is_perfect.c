a871200cd75692a159585ec6d43914ec102413f6
alexa221 <alextakele16@gmail.com>
2022-09-21 16:43:30 +0300

    check BT is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,6 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (0);
 }
+
 /**
  * binary_tree_size - measures the size of a binary tree.
  *@tree: pointer to the root node of the tree to measure the size.
