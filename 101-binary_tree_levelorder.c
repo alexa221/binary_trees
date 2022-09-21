17b1afd5be9097708e00012ef255dac8b83faf6a
alexa221 <alextakele16@gmail.com>
2022-09-21 16:47:45 +0300

    level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -22,6 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (0);
 }
+
 /**
  * print_at_level - print node, especific level
  * @tree: pointer to the root node of the tree to traverse
